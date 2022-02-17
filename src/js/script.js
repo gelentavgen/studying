80527ac03fcb99cd4ffd7ef7a3ec8c3f91fcebe7
Anhelina <angelinalenartovych@gmail.com>
2022-02-17 23:19:18 +0200

    layout with no js

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+alert(1);
+// let answer = confirm('Are you over 18?')
+// console.log(answer)
+// let validation = prompt('Are you over 18?', '');
+// console.log(validation);
+// let answer = confirm('Are you over 18?');
+// if (answer) {
+//   console.log('you are welcome')
+// } else {
+//   console.log('get out')
+// };
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i)
+// };
+function logging(a, b) {
+  console.log(a + b)
+};
+logging(3, 5);
