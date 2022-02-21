ea81e1ee424941bbf04d094962b1e135244252de
Anhelina <angelinalenartovych@gmail.com>
2022-02-21 19:18:26 +0200

    full with modal forms

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,46 @@ $(document).ready(function(){
   }
   toggleSlide('.catalog-item__back');
   toggleSlide('.catalog-item__link');
+
+  //Modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('fast');
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut();
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn();
+    });
+  });
+
+  function validateForm(form) {
+    $(form).validate({
+      rules:{
+        name: 'required',
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Державною мовою, будь ласка",
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        }
+      }
+    });
+  }
+  
+  validateForm('#consultation-form');
+  validateForm('#order form');
+  validateForm('#consultation form');
+
+  $('input[name=phone]').mask("+38 (999) 999-9999");
 });
